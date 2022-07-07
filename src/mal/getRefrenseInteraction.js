61a37ccb91fe907d497dca24749d29319cc247d9
ameliakiara <amelia.kiaraa@gmail.com>
2022-07-07 09:52:15 +0700

    Migrate file to SRC and added TVShow TMDB + Added Anime Reference

---
+++ src/mal/getRefrenseInteraction.js
@@ -0,0 +1,41 @@
+const getAnimeName = require('./getModelAnimeName')
+
+module.exports = {
+  async awaitRefrenseInteraction (msg, member) {
+    const i = await msg
+      .awaitMessageComponent({
+        filter: (m) => m.member.id === member.id,
+        max: 1,
+        time: 6e4
+      })
+      .catch((er) => er)
+
+    if (i instanceof Error) return
+    await i.deferReply().catch((er) => er)
+    i.deleteReply()
+
+    try {
+      switch (i.customId) {
+        case 'refrense-select': {
+          const value = i.values[0]
+
+          const animes = await getAnimeName.awaitModelAnime(value)
+
+          msg.edit({
+            embeds: [animes.embed],
+            components: [animes.row],
+            content: `Anime ${animes.title}`
+          })
+          break
+        }
+      }
+    } catch (er) {
+      i.reply({
+        content:
+          'There is something was wrong, please contact my developer at my support server',
+        ephemeral: true
+      })
+      console.error(er)
+    }
+  }
+}
---
+++ src/mal/getSeasonInteraction.js
@@ -0,0 +1,44 @@
+const Mal = require('mal-scraper')
+const getModelAnimeName = require('./getModelAnimeName')
+
+module.exports = {
+  async awaitSeasonInteraction (msg, member) {
+    const i = await msg
+      .awaitMessageComponent({
+        filter: (m) => m.member.id === member.id,
+        max: 1,
+        time: 6e4
+      })
+      .catch((er) => er)
+
+    if (i instanceof Error) return
+    await i.deferReply().catch((er) => er)
+    i.deleteReply()
+
+    try {
+      // eslint-disable-next-line
+      switch (i.customId) {
+        case 'tv-select': {
+          const value = i.values[0]
+          const res = await Mal.getInfoFromName(value)
+
+          const animes = await getModelAnimeName.awaitModelAnime(value)
+
+          msg.edit({
+            embeds: [animes.embed],
+            content: `${res.title}`,
+            components: [animes.row]
+          })
+          break
+        }
+      }
+    } catch (er) {
+      i.reply({
+        content:
+          'There is something was wrong, please contact my developer at my support server',
+        ephemeral: true
+      })
+      console.error(er)
+    }
+  }
+}
